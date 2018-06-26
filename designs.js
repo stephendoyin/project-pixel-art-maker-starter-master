302f153998d1ef98b31bd40d1aca862fa0c237b1
stephendoyin <stevedoyin@gmail.com>
2018-06-26 16:51:35 +0100

    more changes

--- designs.js
+++ designs.js
@@ -5,9 +5,9 @@ $(function () {
     //makegrill function called on submit and 
     function makeGrid(height, width) {
 
-        let tableRowBeginTag = "<tr>";
-        let tableRowCloseTag = "</tr>";
-        let tableData = "<td></td>";
+        const tableRowBeginTag = "<tr>";
+        const tableRowCloseTag = "</tr>";
+        const tableData = "<td></td>";
         //clears grid whenever makegrid() is called
         let insideTable = "";
         let i = 0;
@@ -22,6 +22,7 @@ $(function () {
         $("#pixelCanvas").html(insideTable);
 
     }
+
     //prevent reload and get value chosen by user,
     //convert to number type and call the makegrid()
     $("#sizePicker").submit(function (event) {
@@ -30,12 +31,12 @@ $(function () {
         let height = Number($("#inputHeight").val());
         makeGrid(height, width);
     });
+
     //click event that uses jquery delegation to get color value
     // and color grid clicked by user
     $("table").on("click", "td", function () {
         let color = $("#colorPicker").val();
         $(this).css("background", color);
     });
-
 
 });
