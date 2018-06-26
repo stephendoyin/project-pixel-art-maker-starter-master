8ee8421b5c2525bb630c31eee5e4fb3e57f6b55d
stephendoyin <stevedoyin@gmail.com>
2018-06-26 16:10:25 +0100

    changes to jsfile

--- designs.js
+++ designs.js
@@ -3,22 +3,24 @@
 $(function () {
 
     //makegrill function called on submit and 
-    //clears grid whenever makegrid() is called
     function makeGrid(height, width) {
-        //tags for inside of html table
+
         let tableRowBeginTag = "<tr>";
         let tableRowCloseTag = "</tr>";
         let tableData = "<td></td>";
+        //clears grid whenever makegrid() is called
         let insideTable = "";
-        //create tags needed for table and insert html format inside of table 
-        for (let i = 0; i < height; i++) {
+        let i = 0;
+        while (i < height) {
             insideTable += tableRowBeginTag;
             for (let x = 0; x < width; x++) {
                 insideTable += tableData;
             }
             insideTable += tableRowCloseTag;
+            i++;
         }
         $("#pixelCanvas").html(insideTable);
+
     }
     //prevent reload and get value chosen by user,
     //convert to number type and call the makegrid()
@@ -27,11 +29,10 @@ $(function () {
         let width = Number($("#inputWidth").val());
         let height = Number($("#inputHeight").val());
         makeGrid(height, width);
-
     });
     //click event that uses jquery delegation to get color value
     // and color grid clicked by user
-    $("table").on("click", "td", function() {
+    $("table").on("click", "td", function () {
         let color = $("#colorPicker").val();
         $(this).css("background", color);
     });
