dd8b81389fc109ef4e7cb4f9f033f2515214190c
stephendoyin <stevedoyin@gmail.com>
2018-06-26 12:27:09 +0100

    fix error

--- designs.js
+++ designs.js
@@ -3,21 +3,20 @@
 
 // When size is submitted by the user, call makeGrid()
 
-
 $(function () {
     //makegrill function call on submit
     function makeGrid(height, width) {
         //tags for inside of html table
         let tableRowBeginTag = "<tr>";
         let tableRowCloseTag = "</tr>";
-        let tabledata = "<td></td>";
+        let tableData = "<td></td>";
         //clears grid whenever makegrid() is called
         let insideTable = "";
         //create tag
-        for (let i = 0; i < width; i++) {
+        for (let i = 0; i < height; i++) {
             insideTable += tableRowBeginTag;
-            for (let x = 0; x < height; x++) {
-                insideTable += tabledata;
+            for (let x = 0; x < width; x++) {
+                insideTable += tableData;
             }
             insideTable += tableRowCloseTag;
         }
@@ -29,7 +28,7 @@ $(function () {
     $("#sizePicker").submit(function (event) {
 
         event.preventDefault();
-        //get value chosen by user and convert to number 
+        //get value chosen by user and convert to number type
         let width = Number($("#inputWidth").val());
         let height = Number($("#inputHeight").val());
         //call the makegrid() function
